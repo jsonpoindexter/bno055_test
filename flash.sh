5665737dba314edff8e6386d0e05b415284e4f18
Jason Poindexter <poindexter.json@gmail.com>
2018-05-19 20:17:52 -0700

    add Ampy commands to copy bnp055 and main files

--- flash.sh
+++ flash.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo esptool.py -p /dev/ttyUSB0 erase_flash
 sudo esptool.py -p /dev/ttyUSB0 write_flash --flash_size=detect -fm dio 0 ~/Downloads/esp8266-20180511-v1.9.4.bin
+sudo ampy -p /dev/ttyUSB0 -d 0 put ./main.py
+sudo ampy -p /dev/ttyUSB0 -d 0 put ./bno055.py
+sudo ampy -p /dev/ttyUSB0 -d 0 put ./functools.py
